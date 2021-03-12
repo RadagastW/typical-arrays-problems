6cdd6d8173202cba86bcd40e156d042813f85dc4
RadagastW <radagastw@yandex.ru>
2021-03-12 14:45:39 +0600

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (array == null || array == '') return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let min = 0;
+    for (let item of array) {
+        min = (item < min) ? item : min;
+    }
+
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == null || array == '') return 0;
+
+    let max = 0;
+    for (let item of array) {
+        max = (item > max) ? item : max;
+    }
+
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == null || array == '') return 0;
+
+    let avg = 0;
+    for (let item of array) {
+        avg += item;
+    }
+    avg = (avg / (array.length));
+
+    return avg;
 }
